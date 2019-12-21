a0a3b80f7f9ddf6c95c051ad41b6f3752a7c16d7
DanPHP7 <danphp7@gmail.com>
2019-12-21 12:50:25 -0300

    feat: rota de hello wolrd

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express();
 app.get('/exemplo', (req, res)=> {
   return res.json({ex: 'Este é um exemplo'})
 })
-
+app.post('/rota', (req, res)=>{
+  return res.json({hello: 'wolrd'})
+})
 app.listen(3333, ()=>{
   console.log('Servidor rodando na porta 3333')
 })
